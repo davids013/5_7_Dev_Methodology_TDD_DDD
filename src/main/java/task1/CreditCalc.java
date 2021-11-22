72190a71b5938ecaed3cee6135dd92ec0b5ce779
Vitaly Shvedko <davids013013@gmail.com>
2021-11-22 23:16:05 +0300

    Test passed realization

--- src/main/java/task1/CreditCalc.java
+++ src/main/java/task1/CreditCalc.java
@@ -1,4 +1,33 @@
 package task1;
 
 public class CreditCalc {
+    private final double loanAmount;
+    private final float rate;
+    private final int months;
+    
+    public CreditCalc(double loanAmount, float rate, int months) {
+        this.loanAmount = loanAmount;
+        this.rate = rate;
+        this.months = months;
+    }
+
+    public double getMonthlyPayment() {
+        double monthRelRate = rate / 12 / 100;
+        double temp = Math.pow(monthRelRate + 1, months);
+        double result = loanAmount * (monthRelRate * temp / (temp - 1));
+        System.out.println("Monthly payment = " + result);
+        return result;
+    }
+
+    public double getOverallDebt() {
+        double result = getMonthlyPayment() * months;
+        System.out.println("Overall debt = " + result);
+        return result;
+    }
+
+    public double getOverpayment() {
+        double result = getOverallDebt() - loanAmount;
+        System.out.println("Overpayment = " + result);
+        return result;
+    }
 }
